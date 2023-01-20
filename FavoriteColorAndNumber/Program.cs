e41e6dfca78c206fc90723f352a4b8be46c03f04
jis1046 <jis1046@rit.edu>
2023-01-20 17:53:33 -0500

    Samuel_PE1

--- FavoriteColorAndNumber/Program.cs
+++ FavoriteColorAndNumber/Program.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace ColorNameSpace
+{
+    namespace Color
+    {
+        //Class: NumberClass
+        //Josh Samuel
+        //Purpose: Class to contain User's favorite color
+        //Restriction: None
+        static class ColorClass
+        {
+            public static string sFavColor = null;
+        }
+    }
+}
+
 namespace FavoriteColorAndNumber
 {
+    //Class: Program
+    //Josh Samuel
+    //Purpose: Console Read/Write and Exception-handling Exercise 
+    //Restriction: None
     static internal class Program
     {
+        //Method: Main
+        //Purpose: Prompt the user for their favorite color and number
+        //         Output  their favorite color (in limited text colors) thier favorite number of times
+        //Restriction: None
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            //Prompt the user for their fav color
+            Console.Write("Enter your favorite color: \t");
+
+            //Read from the console and store that in variable
+            ColorNameSpace.Color.ColorClass.sFavColor = Console.ReadLine();
+
+            //Prompt the user for their fav number
+
+            //Read from the console and store that in variable
+
+            //Change text to match their color
+
+            //Output their fav color and number of times
         }
     }
 }
--- Samuel_HelloWorld/Program.cs
+++ Samuel_HelloWorld/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace Samuel_HelloWorld
 {
+
+        // PE1 Hello World
+        // Josh Samuel
+        // Purpose: Complaing and Running the Program
+        //  Restriction: None
     internal class Program
     {
+        //Main Method
         static void Main(string[] args)
         {
+            //Variables
             String name1 = "Brain";
             int myInteger = 500;
             double myDouble = 5.5;
@@ -17,16 +24,31 @@ namespace Samuel_HelloWorld
             Console.WriteLine("Josh Samuel ");
             Console.WriteLine(myInteger - myDouble);
           
+            //If Statment 
             if (myInteger - myDouble < 500)
             {
                 Console.WriteLine("myInteger is less than 500");
             }
 
-            while(myInteger >= 400)
+            //While Loop
+            Console.WriteLine();
+            Console.WriteLine("While loop");
+
+            while (myInteger > 400)
             {
                myInteger = myInteger - 10;
                 Console.WriteLine(myInteger);
                 
+            }
+
+            //For Loop
+            Console.WriteLine();
+            Console.WriteLine("For loop");
+
+            for (int i = 0; i < 10; i++)
+            {
+                myInteger += 10;
+                Console.WriteLine(myInteger);
             }
 
         }
