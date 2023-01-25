1ffae9e713b79383a35cafae515e2c9bbe8b218b
jis1046 <jis1046@rit.edu>
2023-01-25 00:00:43 -0500

    Samuel PE2

--- FavoriteColorAndNumber/Program.cs
+++ FavoriteColorAndNumber/Program.cs
@@ -44,10 +44,51 @@ namespace FavoriteColorAndNumber
             //Prompt the user for their fav number
 
             //Read from the console and store that in variable
+            sFavNum = Console.ReadLine();
+
+            //note that we cannot pass npFavNum to TryParse became it does not match the method signature 
+            bValid = int.TryParse(sFavNum, out nFavNum);
 
             //Change text to match their color
+            switch(Calias.ColorClass.sFavColor)
+            {
+                case "red":
+                case "Red":
+                case "RED":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
 
+                case "blue":
+                    Console.ForegoundColor = ConsoleColor.Blue;
+                    break;
+
+
+            }
             //Output their fav color and number of times
+            for(int i = 0; i < npFavNum: i +=1)
+            {
+                //Different ways to generate output
+                //Appedning stgrings using the "+" operator
+                Console.WriteLine("Your favorite color is " + Calias.COlorClass.sFAvColor + "! (appending)");
+
+                //Embedding code blocks
+                Console.WriteLine($"Your favoirte color is {Calias.ColorClass.sFavColor + "!"} (interpolation");
+                    {
+                        Calis.ColorClass
+                    }
+            }
+
+            int x, y;
+            y = 1;
+            //x = ++y;
+            //Output:
+            //x = 2
+            //y = 2
+
+            x = y++;
+            //Output:
+            //x = 1
+            //y = 2
         }
     }
 }
---
+++ SquashTheBugs/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquashTheBugs
+{
+    // Class Program
+    // Author: Josh Samuel
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10 
+        //          Output N/(N-1) for all 10 numbers
+        //          and list all numbers processed
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare int counter
+            //Syntax Error: It does not have semicolon
+            //int i = 0
+            int i = 0;
+
+            // loop through the numbers 1 through 10
+            //Logic Error: For loop will run only number 1 through 9
+            //for (i = 1; i < 10; ++i)
+            for (i = 1; i <= 10; ++i)
+            {
+                // declare string to hold all numbers
+                string allNumbers = null;
+
+                // output explanation of calculation
+                //Console.Write(i + "/" + i - 1 + " = ");
+                Console.Write(i + "/" + (i - 1) + " = ");
+
+                // output the calculation based on the numbers
+                //Logic Error it cannot divide by zero
+                //Can't Figure it out
+                //Console.WriteLine(i / (i - 1));
+                Console.WriteLine();
+
+                // concatenate each number to allNumbers
+                allNumbers += i + " ";
+
+                // output all numbers which have been processed
+                Console.WriteLine("These numbers have been processed: " + allNumbers);
+
+                // increment the counter
+                //Logic Error It already use in for loop. This statement would add another increment
+                // i = i + 1;
+                
+
+
+            }
+
+            // output all numbers which have been processed
+            //Syntax Error: It does not have a plus sign between string and variable, also allNumber does not exist outside for loop if allNumber is set in for loop
+            //Console.WriteLine("These numbers have been processed: " allNumbers);
+            
+        }
+    }
+}
