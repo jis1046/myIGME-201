9329f51b306019ceeb83a8df87b4becb7d3ca67d
jis1046 <jis1046@rit.edu>
2023-01-27 15:22:07 -0500

    PE3

---
+++ Samuel_PE3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Samuel_PE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value 1 : ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value 2 : ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value 3 : ");
+            int value3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value 4 : ");
+            int value4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(value1 * value2 * value3 * value4);
+
+        }
+    }
+}
