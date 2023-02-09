04dbda3db9b14e93be2b3544787e1864d496f1bc
jis1046 <jis1046@rit.edu>
2023-02-08 23:30:44 -0500

    Q7

---
+++ PE8 Q7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = "";
+            String reverse = "";
+
+            Console.WriteLine("Type any words");
+            word = Console.ReadLine();
+
+            int l = word.Length - 1;
+
+            for (int i = l; i >= 0; i--)
+            {
+                reverse = reverse + word[i];
+            }
+
+            Console.WriteLine("Words in reverse order");
+            Console.WriteLine(reverse);
+        }
+    }
+}
