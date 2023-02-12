4870747987c80fdb59b3f7f5ba352bd170408811
jis1046 <jis1046@rit.edu>
2023-02-11 22:40:29 -0500

    PE9 Q3

---
+++ PE9 Q3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Josh Samuel
+//PE9 Q3
+//Making delegate function
+namespace PE9_Q3
+{
+    // Step1: declare the delegate
+    public delegate string ReadLineDelegate();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Step 2 Create a delegate variable
+            ReadLineDelegate readline;
+
+            Console.WriteLine("Type the input");
+
+            //Step 3 Assign the readline method to the delegate
+            readline = new ReadLineDelegate(Console.ReadLine);
+
+            //Calling the delegate
+            string readLine = readline();
+
+
+            //Prints delagate output
+            Console.Write("Delagate: ");
+            Console.WriteLine(readLine);
+        }
+    }
+}
---
+++ PE9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            order order1;
+            order1.unitCost= 100;
+            order1.unitCount = 5;
+            Console.WriteLine(order1.totalCost());
+            
+
+        }
+            struct order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+            public double totalCost()
+            {
+                return unitCount * unitCost;
+            }
+
+            
+        }
+
+
+    }
+
+}
+
