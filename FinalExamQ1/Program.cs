a4d3945bd8d93c631e5f528ce4b6b92913f32e2b
jis1046 <jis1046@rit.edu>
2023-05-09 13:55:36 -0400

    FinalExamQ1

---
+++ FinalExamQ1/Program.cs
@@ -0,0 +1,77 @@
+﻿using FinalExamQ1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamQ1
+{
+    public class MyStack
+    {
+
+        List<int> list = new List<int>();
+
+            public void Push(int n)
+            { 
+                list.Add(n); 
+            }
+
+            public int Pop()
+            {
+              int top = 0;
+              int index = list.Count - 1;
+
+            if (index == -1)
+            {
+                Console.WriteLine("ERROR: Empty Stack!");
+            }
+            else
+            {
+                 top = list[index];
+
+                list.RemoveAt(index);
+            }
+
+                return top;
+            }
+        
+            public int Peek()
+            {
+            int top = 0;
+            int index = list.Count - 1;
+            if (index == -1)
+                {
+                Console.WriteLine("ERROR: Empty Stack!");
+                }
+
+            else
+            {
+                top = list[index];
+            }
+
+            return top;
+        }
+    }
+}
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);   
+            myStack.Push(4);
+        
+            Console.WriteLine(myStack.Peek());
+            
+            Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Pop());
+
+        }
+    }
+
