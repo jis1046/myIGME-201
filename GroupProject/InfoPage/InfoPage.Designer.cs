503c565c32fedc0cea59e93ba979bf054e424f54
jis1046 <jis1046@rit.edu>
2023-05-09 22:35:22 -0400

    GroupProject

---
+++ GroupProject/InfoPage/InfoPage.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace InfoPage
+{
+    partial class InfoPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.gradeTextBox = new System.Windows.Forms.TextBox();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(311, 255);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // gradeTextBox
+            // 
+            this.gradeTextBox.Location = new System.Drawing.Point(312, 144);
+            this.gradeTextBox.Name = "gradeTextBox";
+            this.gradeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gradeTextBox.TabIndex = 1;
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeLabel.Location = new System.Drawing.Point(316, 203);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(85, 28);
+            this.gradeLabel.TabIndex = 2;
+            this.gradeLabel.Text = "Grade";
+            this.gradeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(322, 96);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(79, 28);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Red;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGreen;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.startButton.Location = new System.Drawing.Point(308, 314);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(103, 39);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(242, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Climb Info";
+            // 
+            // InfoPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.gradeTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "InfoPage";
+            this.Text = "InfoPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox gradeTextBox;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GroupProject/RBCTimer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBCTimer
+{
+
+
+    public partial class Form1 : Form
+    {
+        TimeSpan timerValue = TimeSpan.Zero;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.button1.Click += new EventHandler(Button1__Click);
+            this.button2.Click += new EventHandler(Button2__Click);
+            this.timer1.Tick += new EventHandler(Timer1__Tick);
+        }
+
+        private void Button1__Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                timer1.Start();
+                button1.Text = "Stop";
+  
+            }
+            else
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+            }
+        }
+
+        private void Button2__Click(object sender, EventArgs e)
+        {
+                // Stop the timer if it's running
+                timer1.Stop();
+
+                // Reset the timerValue variable to zero
+                timerValue = TimeSpan.Zero;
+
+                // Update the Label with the new time
+                label1.Text = timerValue.ToString(@"hh\:mm\:ss");
+
+        }
+
+        private void Timer1__Tick(object sender, EventArgs e)
+        {
+
+                // Add one second to timerValue
+                timerValue = timerValue.Add(TimeSpan.FromSeconds(1));
+
+                // Update the Label with the new time
+                label1.Text = timerValue.ToString(@"hh\:mm\:ss");
+
+        }
+    }
+}
